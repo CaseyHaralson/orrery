caf5652352d05b1277482d0c901a88ccfca6cf5c
Casey <casey.haralson@gmail.com>
2026-01-15 21:52:53 -0600

    feat: add time to greeting

--- hello.js
+++ hello.js
@@ -1,1 +1,15 @@
-console.log('Hello, World!');
+const { getCurrentTime } = require('./lib/time-utils');
+
+function getNameArg(args) {
+  const nameArg = args.find((arg) => arg.startsWith('--name='));
+  if (!nameArg) {
+    return 'World';
+  }
+
+  const value = nameArg.slice('--name='.length).trim();
+  return value || 'World';
+}
+
+const name = getNameArg(process.argv.slice(2));
+const currentTime = getCurrentTime();
+console.log(`Hello ${name}! The current time is ${currentTime}`);
